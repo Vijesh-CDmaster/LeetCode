43ebdc8ecb9045f0f5416b182b593ce5d5d7c297
Vijesh S <233714092+Vijesh-CDmaster@users.noreply.github.com>
2026-09-16 09:18:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (17.85%) - LeetSync

---
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+
+class Solution(object):
+
+  def sumOfUnique(self, nums):
+    """:type nums: List[int]
+
+    :rtype: int
+    """
+    counts = Counter(nums)
+    return sum(num for num, count in counts.items() if count == 1)
